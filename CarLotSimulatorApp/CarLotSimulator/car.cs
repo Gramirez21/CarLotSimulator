f3145220e30237db5349f607edbdc52a0062b24d
Gramirez21 <r.dannysixty@gmail.com>
2024-04-03 22:24:52 -0700

    excercise2

---
+++ CarLotSimulatorApp/CarLotSimulator/car.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLotSimulator
+{
+    public class car
+    {
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+        public bool IsDriveable { get; set; }
+        public string EngineNoise { get; set; }
+        public string HonkNoise { get; set; }
+
+
+
+        public void MakeEngineNoise(string engineNoise)
+        {
+            Console.WriteLine(engineNoise);
+        }
+
+        public void MakeHonkVoice(string honkVoice)
+        {
+            Console.WriteLine(honkVoice);
+        }
+
+        public car(string make, string model, int year, bool isdriveable, string EngineNoise, string HonkNoise)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            IsDriveable = isdriveable;
+        }
+        
+
+
+
+    }
+}
